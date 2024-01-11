a456f9b0142ddf662d5275047e9293a03f1cd0ae
mlancar <slmarine@hotmail.fr>
2024-01-11 12:37:50 +0100

    starting ex05

---
+++ CPP01/ex05/Harl.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malancar <malancar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/11 10:45:28 by malancar          #+#    #+#             */
+/*   Updated: 2024/01/11 10:52:32 by malancar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <iostream>
+
+class   Harl {
+
+    public:
+        Harl();
+        ~Harl();
+    
+        void    complain(std::string level);
+
+    private:
+        void    debug(void);
+        void    info(void);
+        void    warning(void);
+        void    error(void);
+};
+
+#endif
