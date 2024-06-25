6211cc74331323f260f2c230b20244459178f8d8
Marine LANCAR <malancar@bess-f1r3s8.clusters.42paris.fr>
2024-06-25 17:31:18 +0200

    encore une fois je suis trop conne :))))

---
+++ CPP08/ex02/Includes/MutantStack.hpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malancar <malancar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/25 15:39:24 by malancar          #+#    #+#             */
+/*   Updated: 2024/06/25 17:10:19 by malancar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+
+template <class T>
+
+class MutantStack : public std::stack<T> {
+	
+	public:
+		MutantStack() {
+			
+		}
+		~MutantStack() {
+			
+		}
+		
+		T begin() {
+			
+		}
+
+		T end() {
+			
+		}
+		
+	private:
+	
+	
+		
+};
+
+#endif
